cb953d4de70139be3f5fbd6be14477114562d81b
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-25 23:20:14 +0500

    Add files via upload

---
+++ 4. Assignment (Famous Quote)/app.ts
@@ -0,0 +1,9 @@
+// Assignmet-4 (Famous Quote)
+
+let qoute: string=" “A person who never made a mistake never tried anything new.”";
+let author: string="Albert Einstein";
+console.log(`${author} Once said, "${qoute}`);
+
+
+
+
