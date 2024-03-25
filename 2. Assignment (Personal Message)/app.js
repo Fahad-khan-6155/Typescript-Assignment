99bfa5f93022b39b0d495c08cf29fe5a3f5b4752
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-25 21:56:16 +0500

    Add files via upload

---
+++ 2. Assignment (Personal Message)/app.js
@@ -0,0 +1,3 @@
+// Assignment-2 (Personal Message)
+var personName = "Eric";
+console.log("hello ".concat(personName, ", Hello Eric, would you like to learn some Python today?"));
---
+++ 2. Assignment (Personal Message)/app.ts
@@ -0,0 +1,4 @@
+// Assignment-2 (Personal Message)
+
+let personName: string = "Eric";
+console.log(`hello ${personName}, Hello Eric, would you like to learn some Python today?`);
