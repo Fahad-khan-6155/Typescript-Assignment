7864aa989b1346a87c85a27ba5b0d7b6eeab7997
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-25 23:21:20 +0500

    Add files via upload

---
+++ 5. Assignment Famous Quote 2)/app.ts.ts
@@ -0,0 +1,6 @@
+// Assignment-5 (Famous Quote 2)
+
+let qoute: string = "A person who never made a mistake never tried anything new.";
+let famous_person = "Albert Einstein"
+let message =` ${famous_person} once said ${qoute}`;
+console.log(message);
