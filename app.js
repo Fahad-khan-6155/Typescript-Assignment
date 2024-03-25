fd85515fe3901c320174275f7fd18ec367a48211
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-25 17:27:53 +0500

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// Assignmet-4 (Famous Quote)
-var qoute = " “A person who never made a mistake never tried anything new.”";
-var author = "Albert Einstein";
-console.log("".concat(author, " Once said, \"").concat(qoute));
