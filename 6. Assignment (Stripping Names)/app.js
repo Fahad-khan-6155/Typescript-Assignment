a412a76bfadb3a30aa8a49509ca73886dcaefc99
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-25 23:22:58 +0500

    Add files via upload

---
+++ 6. Assignment (Stripping Names)/app.js
@@ -0,0 +1,5 @@
+// Assignment-6 (Stripping Names:)
+var personName = "\n\t Fahad Khan\t\n";
+console.log(personName);
+var Stripped = personName.trim();
+console.log(Stripped);
---
+++ 6. Assignment (Stripping Names)/app.ts.ts
@@ -0,0 +1,6 @@
+// Assignment-6 (Stripping Names:)
+
+let personName: string =`\n\t Fahad Khan\t\n`;
+console.log(personName);
+let Stripped: string = personName.trim();
+console.log (Stripped);
