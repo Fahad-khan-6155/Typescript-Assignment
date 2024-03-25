9c298f2520a6cda3bf220b5afa80c258a183f294
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-25 23:16:57 +0500

    Delete 3 Name_Case.ts

--- 3 Name_Case.ts
+++
@@ -1,9 +0,0 @@
-// lower case 
-let personName: string ="Fahad"
-console.log ("lower case:", personName.toLowerCase());
-
-// Upper case
-console.log("uppercase:" , personName.toUpperCase());
-
-// TITLE CASE
-console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
